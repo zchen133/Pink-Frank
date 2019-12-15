0b06e9adf5a7ae87590efe43e34149163c789022
czhu12 <czhu12@ucsc.edu>
2019-12-15 02:53:54 -0800

    engin fire works

--- Pink Frank/Assets/PlayerController2D.cs
+++ Pink Frank/Assets/PlayerController2D.cs
@@ -7,6 +7,8 @@ public class PlayerController2D : MonoBehaviour
     Animator animator;
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
+    private bool facing = true;
+    private bool jump = true;
     
     // Start is called before the first frame update
     void Start()
@@ -21,14 +23,18 @@ public class PlayerController2D : MonoBehaviour
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2d.velocity = new Vector2(4, rb2d.velocity.y);
-            
-            spriteRenderer.flipX = false;
+            if(facing == false){
+                facing = true;
+                transform.Rotate(0f, 180f, 0f);
+            }
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2d.velocity = new Vector2(-4, rb2d.velocity.y);
-            
-            spriteRenderer.flipX = true;
+            if(facing == true){
+                facing = false;
+                transform.Rotate(0f, 180f, 0f);
+            }
         }
         
         
@@ -36,9 +42,6 @@ public class PlayerController2D : MonoBehaviour
         if (Input.GetKey("space"))
         {
             rb2d.velocity = new Vector2(rb2d.velocity.x, 4);
-            //animator.Play("Frank_engfire");
-            animator.Play("frank_idle");
-
         }
     }
 }
---
+++ Pink Frank/Assets/engfire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class engfire : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            animator.Play("Frank_engfire");
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            animator.Play("Frank_engfire");
+        }
+        else{
+            animator.Play("inistate");
+        }
+        if (Input.GetKey("space"))
+        {
+            animator.Play("Frank_engfire");
+        }
+    }
+}
