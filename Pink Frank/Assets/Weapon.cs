f632352d737ddf6d23f0df45cd257cb25b4e18b8
czhu12 <czhu12@ucsc.edu>
2019-12-15 19:35:48 -0800

    bullet add background change

---
+++ Pink Frank/Assets/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("j")){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        //shoot
+        Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+    }
+}
